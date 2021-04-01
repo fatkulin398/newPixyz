8dbc9a19794092327b296cf82994017371dd55b4
fatkulin398 <fatkulin398@gmail.com>
2021-04-01 15:43:18 +0500

    FINISH I THINK

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ let hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function(){
 hamburger.classList.toggle('open');
 navItems.classList.toggle('open');
-})
+});
+
+
+let priceList = document.querySelector('.price_items_list');
+
+priceList.addEventListener('click', function(){
+    priceList.classList.toggle('active');
+});
+
